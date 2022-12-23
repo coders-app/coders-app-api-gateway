60c47186a672f84d03842b75b45aa27174ab13d1
Lorena Martinez Moledo <lmartmol@gmail.com>
2022-12-23 22:27:35 +0100

    SuperTest pingPongProtocol Router

--- src/server/routes/pingPongProtocolRouter/pingPongProtocolRouter.test.ts
+++ src/server/routes/pingPongProtocolRouter/pingPongProtocolRouter.test.ts
@@ -1,18 +1,22 @@
 import request from "supertest";
-import app from "./app";
-import httpStatusCodes from "../utils/httpStatusCodes";
 
+import app from "../../app";
+import paths from "../paths";
+import httpStatusCodes from "../../../utils/httpStatusCodes";
+
+const { baseUrl } = paths;
 const {
+  successCode: { okCode },
   clientErrors: { notFoundCode },
 } = httpStatusCodes;
 
 describe("Given a GET / endpoint", () => {
   describe("When it receives a request", () => {
     test("Then it should respond with status 200 and message 'Pong 🏓'", async () => {
-      const expectedStatus = 200;
+      const expectedStatus = okCode;
       const expectedMessage = "Pong 🏓";
 
-      const response = await request(app).get("/").expect(expectedStatus);
+      const response = await request(app).get(baseUrl).expect(expectedStatus);
 
       expect(response.body).toHaveProperty("message", expectedMessage);
     });
