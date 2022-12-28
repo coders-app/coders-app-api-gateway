baa3600e6465f477ead55f00a84cfb6970648c50
Mario González <mario@mariogl.com>
2022-12-28 13:53:32 +0100

    Add Basic Auth username&password to environment variables

--- src/loadEnvironments.ts
+++ src/loadEnvironments.ts
@@ -2,9 +2,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { PORT: port, ORIGIN_WHITELIST: originWhitelist } = process.env;
+const {
+  PORT: port,
+  ORIGIN_WHITELIST: originWhitelist,
+  BASIC_AUTH_USERNAME: basicAuthUsername,
+  BASIC_AUTH_PASSWORD: basicAuthPassword,
+} = process.env;
 
 export const environment = {
   port,
   originWhitelist: originWhitelist.split(","),
+  basicAuth: {
+    username: basicAuthUsername,
+    password: basicAuthPassword,
+  },
 };
