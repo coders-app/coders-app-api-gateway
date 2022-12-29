2eb1be54311708f112a1738355d42b40edd4d02c
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2022-12-29 19:43:12 +0100

    Set up tsconfig for json files and exclude swagger files from coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
+  setupFiles: ["./src/setupTests.ts"],
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
   resolver: "jest-ts-webcompat-resolver",
@@ -8,5 +9,8 @@ module.exports = {
     "src/**/*.ts",
     "!src/index.ts",
     "!src/server/startServer.ts",
+    "!src/utils/loadJson.ts",
+    "!src/openapi/*.ts",
+    "!src/setupTests.ts",
   ],
 };
--- src/server/app.ts
+++ src/server/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
 import generalError, { unknownEndpoint } from "./middlewares/errors.js";
 import paths from "./routes/paths.js";
 import pingPongProtocolRouter from "./routes/pingPongProtocolRouter/pingPongProtocolRouter.js";
+import openApiDocument from "../openapi/index.js";
 
-const { baseUrl } = paths;
+const { baseUrl, apiDocs } = paths;
 
 const app = express();
 
@@ -17,6 +19,8 @@ app.use(morgan("dev"));
 app.use(express.json());
 
 app.use(baseUrl, pingPongProtocolRouter);
+
+app.use(apiDocs, swaggerUi.serve, swaggerUi.setup(openApiDocument));
 
 app.use(unknownEndpoint);
 app.use(generalError);
