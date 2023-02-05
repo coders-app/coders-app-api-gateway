880210f6c0a3ed78a7aadc592074ab99b20fa2bb
Mario González <mario@mariogl.com>
2023-02-05 14:12:14 +0100

    Remove MSW and its config

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: "ts-jest",
-  setupFilesAfterEnv: ["./jest.setup.ts"],
   setupFiles: ["./src/setupTests.ts"],
   testEnvironment: "node",
   testMatch: ["**/src/**/*.test.ts"],
