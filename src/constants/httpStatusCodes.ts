ffb6b78fe53e0a22d161ed7aabf1c1c04619fcf8
Benjamin Rae <63359209+benjaminrae@users.noreply.github.com>
2023-02-24 12:22:39 +0100

    Add requests headers object and create constants

---
+++ src/constants/httpStatusCodes.ts
@@ -0,0 +1,16 @@
+const httpStatusCodes = {
+  successCode: {
+    okCode: 200,
+  },
+
+  clientErrors: {
+    badRequestCode: 400,
+    notFoundCode: 404,
+  },
+
+  serverErrors: {
+    internalServerErrorCode: 500,
+  },
+};
+
+export default httpStatusCodes;
---
+++ src/constants/requestHeaders.ts
@@ -0,0 +1,8 @@
+const requestHeaders = {
+  apiKey: "X-API-KEY",
+  apiName: "X-API-NAME",
+  contentType: "Content-Type",
+  contentLength: "Content-Length",
+};
+
+export default requestHeaders;
